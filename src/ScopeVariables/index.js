0845cba01c40bd363ac799753e0af90304850743
Mohanraj <mohanraj@skava.com>
2019-04-03 18:26:02 +0530

    scope variables file included
    - index.js removed
    - some examples included

--- src/ScopeVariables/index.js
+++ src/ScopeVariables/index.js
@@ -1,5 +1,31 @@
 const a = 10;
 
+/**
+ * @error
+ * TypeError: Assignment to constant variable.
+ */
+// a = 20;
+
+/**
+ * @error
+ * SyntaxError: Identifier 'a' has already been declared
+ */
+
 // let a = 10;
+// var a = 10;
+
+function doTestScopeVariables() {
+  var a = 1;
+
+  /**
+   * @error
+   * SyntaxError: Identifier 'a' has already been declared
+   */
+  //   let a = 2;
+  //   console.log("let: " + a);
+
+  console.log("var: " + a);
+}
 
 console.log(a);
+doTestScopeVariables();
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const abc = 10;
-
-const func = (a, b) => a + b;

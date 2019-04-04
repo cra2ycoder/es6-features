c280b2372f5a874580438adf9f5a599077793c7a
Mohanraj <mohanraj@skava.com>
2019-04-04 17:12:06 +0530

    array destructuring incl.

---
+++ src/destructuring/README.md
@@ -0,0 +1,5 @@
+# Destructuring
+
+- its a cool feature from ES6
+- its an javascript expression
+- that helps to unpack the values from Array and Object - to distinct variables
---
+++ src/destructuring/array.js
@@ -0,0 +1,8 @@
+// array destructuring
+const [a, b, c, d, ...rest] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(...rest);

02cc95e15000345486562ed7dc9c401fc8e3d269
Mohanraj <mohanraj@skava.com>
2019-04-14 18:07:04 +0530

    additional examples included
    - concurrent
    - sequential

--- src/async-await/async-await.js
+++ src/async-await/async-await.js
@@ -23,4 +23,96 @@ async function toDo() {
     const res = await getListFromUser()
     console.log(res)
 }
-toDo()
+
+// toDo()
+
+/**
+ * @function
+ *
+ */
+const fastPromiseFunction = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('fastPromiseFunction')
+        }, 200)
+    })
+}
+
+const slowPromiseFunction = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('slowPromiseFunction')
+        }, 500)
+    })
+}
+
+/**
+ * @example @function
+ * for timer conversion
+ */
+
+const getTimer = (curTime, prevTime) => {
+    const time = curTime - prevTime
+    return time / 1000
+}
+
+/**
+ * @example
+ * SEQUENTIAL
+ * - start one by one
+ */
+
+const sequentialFlow = async () => {
+    let slowTimer = new Date().getTime()
+    const slow = await slowPromiseFunction()
+
+    let fastTimer = new Date().getTime()
+    const fast = await fastPromiseFunction()
+
+    console.log(slow) // slowPromiseFunction
+    console.log(getTimer(new Date().getTime(), slowTimer))
+
+    console.log('/////////////////////////////')
+
+    console.log(fast) // fastPromiseFunction
+    console.log(getTimer(new Date().getTime(), fastTimer))
+}
+
+// sequentialFlow()
+
+/**
+ * @example
+ * CONCURRENT
+ * - start at the same time
+ */
+const concurrentFlow = async () => {
+    let slowTimer = new Date().getTime()
+    const slow = slowPromiseFunction()
+
+    let fastTimer = new Date().getTime()
+    const fast = fastPromiseFunction()
+
+    await slow
+    console.log(getTimer(new Date().getTime(), slowTimer))
+
+    console.log('/////////////////////////////')
+
+    await fast
+    console.log(getTimer(new Date().getTime(), fastTimer))
+}
+
+// concurrentFlow()
+
+/**
+ * @example
+ */
+
+const concurrentFlowWithPromiseAll = async () => {
+    return Promise.all([slowPromiseFunction(), fastPromiseFunction()])
+        .then(msg => {
+            console.log(msg)
+        })
+        .catch(err => err)
+}
+
+concurrentFlowWithPromiseAll()

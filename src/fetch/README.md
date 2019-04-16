8d65d4d96a6bf1b6f451131020a3499cf0f7bb74
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-16 12:42:50 +0530

    added two more fetch examples

--- src/fetch/README.md
+++ src/fetch/README.md
@@ -19,3 +19,20 @@ yarn add node-fetch
     ex: `http://api.icndb.com/jokes/random/10`
 -   it returns `Promise` object to get the response
 -   we can pass `init` options as a second argument
+
+## syntax
+
+```
+fetch(url, options)
+    .then(res => res.json()) // where we getting the entire response
+    .then(data => {
+        console.log(data) // where we get the particular data that we need
+    })
+    .catch(err => err) // error
+```
+
+## Reference Link
+
+-   https://github.github.io/fetch/
+-   https://www.npmjs.com/package/node-fetch
+-   https://developers.google.com/web/updates/2015/03/introduction-to-fetch
--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -31,15 +31,56 @@ function getError(err) {
  * using fetch
  */
 
-fetch(resourceURL)
-    .then(res => {
-        console.log(res)
-        console.log(res.status)
-        // Examine the text in the response
-        res.json().then(function(data) {
+function fetchExample1() {
+    fetch(resourceURL)
+        .then(res => {
+            console.log(res)
+            console.log(res.status)
+            // Examine the text in the response
+            res.json().then(function(data) {
+                console.log(data)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+// fetchExample1()
+
+console.log('/////////////////////////////////')
+
+function fetchExample2() {
+    fetch(resourceURL)
+        .then(res => {
+            // console.log(res)
+
+            console.log(res.url)
+            console.log(res.status)
+            console.log(res.statusText)
+
+            /**
+             * @note
+             * get header response
+             */
+            console.log(res.headers)
+            console.log(res.headers.get('server'))
+
+            /**
+             * @important @note
+             * res.json() should be return to receive the data in another then()
+             *
+             * */
+            return res.json()
+        })
+        .then(data => {
             console.log(data)
         })
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+// fetchExample2()
+
+console.log('/////////////////////////////////')
